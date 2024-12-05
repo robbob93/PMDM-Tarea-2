3be276e438a3878b533d634b3adc60b187e5aab7
Roberto Linares <roblinares45@gmail.com>
2024-12-05 16:17:09 +0100

    Creada navigation drawer que no oculta toolbar pero la oscurece

--- ListadoPersonajes/app/src/main/java/linares/rodriguez/listadopersonajes/MainActivity.java
+++ ListadoPersonajes/app/src/main/java/linares/rodriguez/listadopersonajes/MainActivity.java
@@ -7,20 +7,25 @@ import android.view.MenuItem;
 import android.view.View;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.splashscreen.SplashScreen;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -37,7 +42,8 @@ import linares.rodriguez.listadopersonajes.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private ActionBarDrawerToggle toggle;
+    private ActivityMainBinding binding;
     private NavController navController;
 
     @Override
@@ -46,23 +52,71 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //EdgeToEdge.enable(this);
 
-
-        ActivityMainBinding binding  = ActivityMainBinding.inflate(getLayoutInflater());
+        binding  = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        // Obtener el NavController desde el NavHostFragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        NavHostFragment navHostFragment = (NavHostFragment) fragmentManager.findFragmentById(R.id.nav_host_fragment);
+        navController = navHostFragment.getNavController();
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController);
+
+
+        // Configurar menú toggle
+        configureToggleMenu();
+
+        // Configurar la navegación
+        configureNavigation();
+
 
 //        if (toolbar != null){
 //            toolbar.setTitle("Titulo");
 //            toolbar.setSubtitle("Subtitulo");
 //        }
         simpleSnackbar(findViewById(R.id.nav_host_fragment));
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
     }
+
+
+    private void configureNavigation() {
+        NavigationUI.setupWithNavController(binding.navView, navController);
+
+        // Manejar la selección de elementos del menú
+        binding.navView.setNavigationItemSelectedListener(menuItem -> {
+            if (menuItem.getItemId() == R.id.nav_home) {
+                navController.navigate(R.id.mainFragment); // Navegar al fragmento de inicio
+            }
+            binding.drawerLayout.closeDrawers(); // Cerrar el menú
+            return true;
+        });
+
+        // Maneja la opción de perfil del header del menú
+        ImageView profileImageView = binding.navView.getHeaderView(0).findViewById(R.id.header_image);
+
+        profileImageView.setOnClickListener(v -> {
+            navController.navigate(R.id.settingsFragment); // Navegar al fragmento de perfil
+            binding.drawerLayout.closeDrawers(); // Cerrar el menú
+        });
+    }
+
+
+    private void configureToggleMenu() {
+        // Configurar el ActionBarDrawerToggle
+        toggle = new ActionBarDrawerToggle(
+                this,
+                binding.drawerLayout,
+                R.string.open_drawer,
+                R.string.close_drawer
+        );
+        binding.drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
 
 
     public void pjClicked(PjData pj, View view) {
@@ -75,10 +129,21 @@ public class MainActivity extends AppCompatActivity {
         Navigation.findNavController(view).navigate(R.id.pjDetailFragment, bundle);
     }
 
+//    @Override
+//    public boolean onSupportNavigateUp() {
+//        // Utiliza el método navigateUp del NavController
+//        return navController.navigateUp() || super.onSupportNavigateUp();
+//    }
+
     @Override
     public boolean onSupportNavigateUp() {
-        // Utiliza el método navigateUp del NavController
-        return navController.navigateUp() || super.onSupportNavigateUp();
+        Fragment navHostFragment = getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+
+        if (navHostFragment != null) {
+            NavController navController = NavHostFragment.findNavController(navHostFragment);
+            return NavigationUI.navigateUp(navController, binding.drawerLayout) || super.onSupportNavigateUp();
+        }
+        return super.onSupportNavigateUp();
     }
 
     public void simpleSnackbar(View view){

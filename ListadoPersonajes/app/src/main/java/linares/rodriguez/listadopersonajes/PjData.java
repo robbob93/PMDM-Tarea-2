23ff3eeb3199b039f2f69048aa18a5c6c3042835
Roberto Linares <roblinares45@gmail.com>
2024-12-10 19:47:07 +0100

    comentadas clases MainActivity y PjData

--- ListadoPersonajes/app/src/main/java/linares/rodriguez/listadopersonajes/PjData.java
+++ ListadoPersonajes/app/src/main/java/linares/rodriguez/listadopersonajes/PjData.java
@@ -1,5 +1,10 @@
 package linares.rodriguez.listadopersonajes;
 
+
+/**
+ * Clase PjData que representa los datos de un personaje en la aplicación.
+ * Incluye información sobre la imagen, el nombre, la descripción y la habilidad del personaje.
+ */
 public class PjData {
 
     private final int image;
@@ -7,6 +12,15 @@ public class PjData {
     private final String description;
     private final String skill;
 
+
+    /**
+     * Constructor de la clase PjData.
+     *
+     * @param image       ID del recurso de la imagen del personaje.
+     * @param name        Nombre del personaje.
+     * @param description Descripción del personaje.
+     * @param skill       Habilidad especial del personaje.
+     */
     public PjData(int image, String name, String description, String skill) {
         this.image = image;
         this.name = name;
@@ -14,19 +28,38 @@ public class PjData {
         this.skill = skill;
     }
 
-
+    /**
+     * Obtiene el ID de recurso de la imagen del personaje.
+     *
+     * @return ID de recurso de la imagen.
+     */
     public int getImage() {
         return image;
     }
 
+    /**
+     * Obtiene el nombre del personaje.
+     *
+     * @return Nombre del personaje.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Obtiene la descripción del personaje.
+     *
+     * @return Descripción del personaje.
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Obtiene la habilidad especial del personaje.
+     *
+     * @return Habilidad especial del personaje.
+     */
     public String getSkill() {
         return skill;
     }
